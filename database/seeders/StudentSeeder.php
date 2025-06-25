d6a1c405ad39fe089e195da9da66f61e475e4a13
dieudonne <testkathembo@gmail.com>
2025-06-25 10:20:09 +0300

    add additional student seeder in the seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -31,5 +31,24 @@ class StudentSeeder extends Seeder
                 ])->assignRole('Student');
             }
         }
+
+        // Uncomment the following lines to create additional users for each program
+
+        
+        // foreach ($programs as $program => $school) {
+        //     // Start from 400 and create 130 users (400-529)
+        //     for ($i = 401; $i < 530; $i++) {
+        //         User::create([
+        //             'first_name' => 'Student' . $i,
+        //             'last_name' => $program,
+        //             'email' => strtolower($program) . $i . '@strathmore.edu',
+        //             'phone' => '07013' . str_pad($i, 4, '0', STR_PAD_LEFT),
+        //             'code' => strtoupper($program) . str_pad($i, 4, '0', STR_PAD_LEFT),
+        //             'schools' => $school,
+        //             'programs' => $program,
+        //             'password' => Hash::make('password'),
+        //         ])->assignRole('Student');
+        //     }
+        // }
     }
 }
